e9c33cdc2a9aefd8d5169d2f48a6366088fe1126
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-05 21:14:11 -0500

    fix

--- lib/table.sh
+++ lib/table.sh
@@ -19,7 +19,8 @@ p6_pgsql_filter_pipe_to_table() {
     local line
     while read -r line; do
         if ! p6_string_blank "$line"; then
-            local values=$(p6_echo "$line" | p6_filter_translate_blank_to_null | p6_filter_sql_escape_single_quote | p6_filter_translate_words_to_sql_list "$sep")
+            local values
+            values=$(p6_echo "$line" | p6_filter_translate_blank_to_null | p6_filter_sql_escape_single_quote | p6_filter_translate_words_to_sql_list "$sep")
             p6_echo "INSERT INTO $table ($column_list) VALUES $values;"
         fi
     done |
